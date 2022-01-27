bfb02ac28c6fdb96a57acf91451a782246760969
Eduardo Lopes <eduardolopes@192.168.1.6>
2022-01-27 01:10:13 -0300

    Setting initial responsivity config on Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,21 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean
+}
+
+export function Profile( { showProfileData = true }: ProfileProps ) {
     return(
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Eduardo Lopes</Text>
-                <Text color="gray.300" fontSize="small">
-                    eduardoloppes@gmail.com
-                </Text>
-            </Box>
+            { showProfileData && (
+                
+                <Box mr="4" textAlign="right">
+                    <Text>Eduardo Lopes</Text>
+                    <Text color="gray.300" fontSize="small">
+                        eduardoloppes@gmail.com
+                    </Text>
+                </Box>
+            )}
 
             <Avatar size="md" name="Eduardo Lopes" src="https://github.com/eduardolopes00.png" />
         </Flex>
