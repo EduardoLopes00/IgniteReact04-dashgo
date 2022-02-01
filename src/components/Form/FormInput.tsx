0bffbc0c55a3bc7b9d0a5e29717653ac52b581fb
Eduardo Lopes <eduardolopes@192.168.1.16>
2022-01-31 23:53:02 -0300

    Creating form validations

--- src/components/Form/FormInput.tsx
+++ src/components/Form/FormInput.tsx
@@ -1,14 +1,19 @@
-import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui/react';
-
+import { FormErrorMessage, FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui/react';
+import { forwardRef, ForwardRefRenderFunction } from 'react'
+import { FieldError } from 'react-hook-form';
 interface InputProps extends ChakraInputProps {
     name: string;
     label?: string;
+    error?: FieldError;
 }
 
 
-export function FormInput({ name, label, ...rest }: InputProps) {
+const FormInputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
+  = ({ name, label, error = null, ...rest }: InputProps, ref) => {
+
+
     return (
-        <FormControl>
+        <FormControl isInvalid={!!error}>
             { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
 
             <ChakraInput
@@ -21,10 +26,19 @@ export function FormInput({ name, label, ...rest }: InputProps) {
                 bgColor: 'gray.900'
               }}
               size="lg"
+              ref={ref}
               {...rest} //Sinalizing that i will use all the other properties provided by ChakraInput. In this case, the propery "type"
             />
+
+            { error && (
+              <FormErrorMessage>
+                {error.message}
+              </FormErrorMessage>
+            ) }
 
           </FormControl>
     )
 
 }
+
+export const FormInput = forwardRef(FormInputBase) //This allows us to pass Ref's to a chiild component
