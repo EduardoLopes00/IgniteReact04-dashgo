8b14cb5afcf8d96519a2e65716c239a1baf0d3dd
Eduardo Lopes <eduardolopes@Eduardo-Lopes-MacBook-Pro-503A.local>
2022-02-02 18:13:37 -0300

    Configuring React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,17 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme' 
+
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { makeServer } from '../services/mirage'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
+
+const queryClient = new QueryClient();
+
 
 function MyApp({ Component, pageProps }: AppProps  ) {
   return (
@@ -10,9 +20,11 @@ function MyApp({ Component, pageProps }: AppProps  ) {
     //This allows that we use all the chakra themes.
     //resetCSS will remove all the default styles from HTML tags
     <ChakraProvider resetCSS theme={theme}> 
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
+      <QueryClientProvider client={queryClient}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,16 +1,34 @@
-import { Box, Flex, Heading, Button, Icon, Text, Table, Thead, Tbody, Th, Tr, Td, Checkbox, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Spinner, Flex, Heading, Button, Icon, Text, Table, Thead, Tbody, Th, Tr, Td, Checkbox, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import Link from 'next/link'
+import { useEffect } from "react";
+import { useQuery } from "react-query";
 
 export default function UserList() {
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
     })
+
+    const query = useQuery('users', async () => {
+        const response = await fetch('http://localhost:3000/api/users')
+        const data = await response.json()
+
+        return data;
+    })
+
+    console.log(query);
+
+    useEffect(() => {
+        fetch('https://localhost:3000/apiFake/users').then(response => response.json())
+                                                 .then(data => data)
+    }, [])
     
+    const { data, isLoading, error } = query;   
+
     return (
         <Box>
             <Header />
@@ -37,49 +55,65 @@ export default function UserList() {
                         </Link>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha" >
-                        <Thead>
-                            <Tr>
-                                <Th px= {["4", "4", "6"]} color="gray.300" width="8"> 
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                { isWideVersion && <Th>Data de cadastro</Th> }
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px= {["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Eduardo Lopes</Text>
-                                        <Text fontSize="sm" color="gray.300">eduardo.toworkstudy@gmail.com</Text>
+                    
 
-                                    </Box>
-                                </Td>
-                                { isWideVersion && <Td>  26 de janeiro de 2021</Td> }
-                                <Td>
-                                    {isWideVersion && 
-                                        <Button
-                                            as="a"
-                                            size="sm"
-                                            fontSize="sm"
-                                            colorScheme="pink"
-                                            leftIcon={<Icon as={RiPencilLine} fontSize="18"/>}
-                                        >  
-                                            Editar
-                                        </Button> 
-                                    }
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-
-                    <Pagination />
                 </Box> 
+                
+                {   isLoading ? (
+                    <Flex justify="center">
+                        <Spinner />
+                    </Flex>
+                ) : error ? (
+                    <Flex justify="center">
+                        <Text>Falha ao obter os usuários!</Text>
+                    </Flex>
+                ) : (
+                    <>
+                        <Table colorScheme="whiteAlpha" >
+                            <Thead>
+                                <Tr>
+                                    <Th px= {["4", "4", "6"]} color="gray.300" width="8"> 
+                                        <Checkbox colorScheme="pink" />
+                                    </Th>
+                                    <Th>Usuário</Th>
+                                    { isWideVersion && <Th>Data de cadastro</Th> }
+                                </Tr>
+                            </Thead>
+                            <Tbody>
+                                <Tr>
+                                    <Td px= {["4", "4", "6"]}>
+                                        <Checkbox colorScheme="pink" />
+                                    </Td>
+                                    <Td>
+                                        <Box>
+                                            <Text fontWeight="bold">Eduardo Lopes</Text>
+                                            <Text fontSize="sm" color="gray.300">eduardo.toworkstudy@gmail.com</Text>
 
+                                        </Box>
+                                    </Td>
+                                    { isWideVersion && <Td>  26 de janeiro de 2021</Td> }
+                                    <Td>
+                                        {isWideVersion && 
+                                            <Button
+                                                as="a"
+                                                size="sm"
+                                                fontSize="sm"
+                                                colorScheme="pink"
+                                                leftIcon={<Icon as={RiPencilLine} fontSize="18"/>}
+                                            >  
+                                                Editar
+                                            </Button> 
+                                        }
+                                    </Td>
+                                </Tr>
+                            </Tbody>
+                        </Table>
+
+                        <Pagination />
+                    </>
+                )
+
+                }
             </Flex>
         </Box>
     )
